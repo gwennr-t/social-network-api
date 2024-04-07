dc7260287b50644e7adf1c4c85e422735e5c0377
Gwenn RT <gwenncreative@2020-MBP.attlocal.net>
2024-04-07 00:51:48 -0500

    outlined userController and thoughtController files

---
+++ controllers/thoughtController.js
@@ -0,0 +1,17 @@
+const { User, Thought } = require('../models');
+
+module.exports = {
+    // get all thoughts
+
+    // get a single thought by its _id
+
+    // create a new thought
+
+    // update a thought by its _id
+
+    // remove a thought by its _id
+
+    // create a reaction stored in a single thoughts reactions array field
+    
+    // pull and remove a reaction by the reaction's reactionId value
+}
---
+++ controllers/userController.js
@@ -0,0 +1,18 @@
+const { UserId } = require('mongoose').Types;
+const { User, Thought } = require('../models');
+
+module.exports = {
+    // get all users
+
+    // get a single user by its _id and populated thought and friend data
+
+    // create a new user
+
+    // update a user by its _id
+
+    // remove a user by its _id
+
+    // add a new friend toa user's friend list
+    
+    // remove a user by its _id
+}
